0ef5068939b5a6947ce818b8773abd02a796a6d6
Salman Raza <156891028+salmanrazaa@users.noreply.github.com>
2025-03-15 15:27:19 +0530

    Create resources.tf

---
+++ resources.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "jen1" {
+    ami = "ami-04b4f1a9cf54c11d0"
+    key_name = "terraformkeypair"
+    instance_type = "t2.micro"
+
+}
+
+source "aws_instance" "jen2" {
+    ami = "ami-04b4f1a9cf54c11d0"
+    key_name = "terraformkeypair"
+    instance_type = "t2.micro"
+
+}
